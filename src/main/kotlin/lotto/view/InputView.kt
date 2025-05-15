313c4002ef1dd5fca1210c1812740bb9b8b35ab5
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-05-15 22:15:32 +0900

    feat: 입력 기능 구현

---
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -0,0 +1,18 @@
+package lotto.view
+
+class InputView {
+    fun readLottoAmount(): String {
+        println("구매 금액을 입력해 주세요.")
+        return readln()
+    }
+
+    fun readWinningNumbers(): String {
+        println("당첨 번호를 입력해 주세요.")
+        return readln()
+    }
+
+    fun readBonusNumber(): String {
+        println("보너스 번호를 입력해 주세요.")
+        return readln()
+    }
+}
