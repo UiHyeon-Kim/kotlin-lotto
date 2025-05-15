1e8a3c62711bf679ff54380aef4fb30dc1c0366f
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-05-15 22:17:20 +0900

    feat: 입력 검증 구현

---
+++ src/main/kotlin/lotto/utils/Validator.kt
@@ -0,0 +1,16 @@
+package lotto.utils
+
+object Validator {
+    fun validateAmount(input: String): Int {
+        val amount = input.toIntOrNull() ?: throw IllegalArgumentException("[ERROR] 구매 금액은 숫자여야 합니다.")
+        require(amount % 1000 == 0) { "[ERROR] 구매 금액은 1000원 단위여야 합니다." }
+        return amount
+    }
+
+    fun validateBonusNumber(input: String, winningNumbers: List<Int>): Int {
+        val number = input.toIntOrNull() ?: throw IllegalArgumentException("[ERROR] 보너스 번호는 숫자여야 합니다.")
+        require(number in 1..45) { "[ERROR] 보너스 번호는 1~45 이내여야 합니다." }
+        require(number !in winningNumbers)
+        return number
+    }
+}
