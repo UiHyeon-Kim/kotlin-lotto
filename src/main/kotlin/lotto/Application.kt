544ead93fb4498f347f28656c073afafdfdee6c5
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-05-16 20:28:01 +0900

    feat: 컨트롤러에 뷰 의존성 주입 및 실행 코드 작성

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,12 @@
 package lotto
 
-fun main() {
+import lotto.presentation.controller.LottoController
+import lotto.presentation.view.InputView
+import lotto.presentation.view.OutputView
 
+fun main() {
+    val inputView = InputView()
+    val outputView = OutputView()
+    val lottoController = LottoController(inputView, outputView)
+    lottoController.run()
 }
