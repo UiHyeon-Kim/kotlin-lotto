4d4dc214eaea0feaa0d60c4adfff2999f58d4594
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-05-16 16:47:26 +0900

    feat: WinningNumbers 검증 함수 추가

--- src/main/kotlin/lotto/domain/utils/Validator.kt
+++ src/main/kotlin/lotto/domain/utils/Validator.kt
@@ -7,6 +7,9 @@ object Validator {
         return amount
     }
 
+    fun validateWinningNumbers(input: String): List<Int> =
+        input.split(",").map { it.toIntOrNull() ?: throw IllegalArgumentException("[ERROR] 당첨 번호는 숫자여야 합니다.") }
+
     fun validateBonusNumber(input: String, winningNumbers: List<Int>): Int {
         val number = input.toIntOrNull() ?: throw IllegalArgumentException("[ERROR] 보너스 번호는 숫자여야 합니다.")
         require(number in 1..45) { "[ERROR] 보너스 번호는 1~45 이내여야 합니다." }
