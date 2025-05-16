e69291915dd1a487f98b59a4e90d4de3ea7771a6
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-05-17 01:03:46 +0900

    refactor: PurchaseLottoUseCase 내부 함수롤 invoke 로 변경

--- src/main/kotlin/lotto/application/usecase/PurchaseLottoUseCase.kt
+++ src/main/kotlin/lotto/application/usecase/PurchaseLottoUseCase.kt
@@ -4,7 +4,7 @@ import lotto.domain.model.Lotto
 import lotto.domain.model.LottoFactory
 
 class PurchaseLottoUseCase {
-    fun getLotteries(amount: Int): List<Lotto> {
+    operator fun invoke(amount: Int): List<Lotto> {
         val count = amount / 1000
         return List(count) { LottoFactory.random() }
     }
