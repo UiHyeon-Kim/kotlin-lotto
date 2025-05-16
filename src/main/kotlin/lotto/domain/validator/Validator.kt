c81a0c27d156fd5d4613d90dd15b6abfa657e0f4
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-05-16 20:30:49 +0900

    refactor: 당첨 번호 검증 코드 추가 수정

--- src/main/kotlin/lotto/domain/validator/Validator.kt
+++ src/main/kotlin/lotto/domain/validator/Validator.kt
@@ -7,8 +7,15 @@ object Validator {
         return amount
     }
 
-    fun validateWinningNumbers(input: String): List<Int> =
-        input.split(",").map { it.toIntOrNull() ?: throw IllegalArgumentException("[ERROR] 당첨 번호는 숫자여야 합니다.") }
+    fun validateWinningNumbers(input: String): List<Int> {
+        val numbers = input.split(",").map {
+            it.toIntOrNull() ?: throw IllegalArgumentException("[ERROR] 당첨 번호는 숫자여야 합니다.")
+        }
+        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.distinct().size == numbers.size)
+        require(numbers.all { it in 1..45 })
+        return numbers
+    }
 
     fun validateBonusNumber(input: String, winningNumbers: List<Int>): Int {
         val number = input.toIntOrNull() ?: throw IllegalArgumentException("[ERROR] 보너스 번호는 숫자여야 합니다.")
