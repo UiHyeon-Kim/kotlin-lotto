d9b270a25a31d68290f7ff7d2c583767ae3e673b
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-05-17 01:38:42 +0900

    feat: 수익률 계산 함수 구현

--- src/main/kotlin/lotto/application/usecase/EvaluateLottoUseCase.kt
+++ src/main/kotlin/lotto/application/usecase/EvaluateLottoUseCase.kt
@@ -4,7 +4,11 @@ import lotto.domain.model.Lotto
 import lotto.domain.model.LottoRank
 
 class EvaluateLottoUseCase {
-    operator fun invoke(myLotteries: List<Lotto>, winning: Lotto, bonusNumber: Int): Map<LottoRank, Int> {
+    operator fun invoke(
+        myLotteries: List<Lotto>,
+        winning: Lotto,
+        bonusNumber: Int
+    ): Map<LottoRank, Int> {
         val result = mutableMapOf<LottoRank, Int>()
         myLotteries.forEach { lotto ->
             val matchCount = lotto.numbers.count { it in winning.numbers }
@@ -13,5 +17,13 @@ class EvaluateLottoUseCase {
             currentRank?.let { result.merge(it, 1) { oldValue, _ -> oldValue + 1 } }
         }
         return result
+    }
+
+    fun calculateReturnRate(
+        results: Map<LottoRank, Int>,
+        purchaseAmount: Int
+    ): Double {
+        val profit = results.entries.sumOf { (rank, count) -> rank.prizeAmount * count }
+        return profit.toDouble() / purchaseAmount * 100
     }
 }
